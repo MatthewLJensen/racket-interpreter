df7270d4007e63755bbfd8ac52186bca0cac96c2
MatthewLJensen <matthewljensen@gmail.com>
2022-04-19 10:58:50 -0400

    Trying to fix parser to work with multiple lines of racket

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,4 @@
+//Parser is somewhat translated from http://norvig.com/lispy.html
 export class Parser {
     private tokens: string[]
 
@@ -5,25 +6,22 @@ export class Parser {
         this.tokens = tokens
     }
 
-    parse() {
-        // read the tokens and build the AST
-        return this.program(this.tokens)
-    }
-
-    program(tokens_to_parse: string[]): any {
+    parse(tokens_to_parse: string[] = this.tokens): any {
         if (tokens_to_parse.length === 0) {
             console.log("ERROR: unexpected end of program")
             return null
         }
 
         let token: string = tokens_to_parse.shift() as any
+        let parse_array: any[] = []
 
         if (token === "(") {
-            let inner_tokens = []
-            while (tokens_to_parse[0] !== ")")
-                inner_tokens.push(this.program(tokens_to_parse))
-            tokens_to_parse.shift() // remove the ")"
-            return inner_tokens
+            let {inner_tokens, shortened_tokens} = this.grouping(tokens_to_parse)
+            console.log(inner_tokens)
+            console.log(shortened_tokens)
+            parse_array.push(inner_tokens)
+            tokens_to_parse = shortened_tokens as any
+            if (tokens)
         }
         else if (token === ")") {
             console.log("ERROR: unexpected )")
@@ -31,6 +29,14 @@ export class Parser {
         }
         else
             return this.identifier(token)
+    }
+
+    grouping(tokens_to_parse: string[]) {
+        let inner_tokens = []
+        while (tokens_to_parse[0] !== ")")
+            inner_tokens.push(this.parse(tokens_to_parse))
+        let shortened_tokens = tokens_to_parse.shift() // remove the ")"
+        return {inner_tokens, shortened_tokens}
     }
 
     identifier(token: string | any) {
