65a7ea97c930c23bf8406dd3fc675920b3ac625e
MatthewLJensen <matthewljensen@gmail.com>
2022-04-20 13:50:22 -0400

    Removed unnecessary files

--- src/num.ts
+++
@@ -1,9 +0,0 @@
-export class Num {
-    value: number
-    constructor(value: number) {
-        this.value = value;
-    }
-    toString() {
-        return this.value.toString();
-    }
-}
--- src/scripts/fact100.rkt
+++
@@ -1,4 +0,0 @@
-(define (factorial base)
-    (cond ((= base 1) 1)
-          (else (* base (factorial (- base 1))))))
-(print (factorial 5))
